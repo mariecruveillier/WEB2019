5f90eb0e069fe28d3f99e92d38234a4be0ddc9cd
mariecruveillier <mariecruveillier@gmail.com>
2019-04-27 14:51:48 +0200

    echartjs map3D tentative

--- src/components/mainComponent.js
+++ src/components/mainComponent.js
@@ -2,6 +2,7 @@ import { h } from 'hyperapp'
 import RadarChart from './radarChart'
 import BubbleChart from './bubbleChart'
 import TreeMapChart from './treeMapChart'
+import Globe from './globe'
 
 /*
   Component that display the main page of the dashboard
@@ -13,7 +14,7 @@ export default (props) =>
     </section>
     <section id='mainGraphs'>
       <div id='map'>
-        <TreeMapChart data={{compGroup: props.data.compGroup, setCompGroups: props.data.setCompGroups, setErrorMess: props.data.setErrorMess}} id='map'/>
+        <Globe />
       </div>
       <BubbleChart />
       <RadarChart data={{compGroup: props.data.compGroup, setCompGroups: props.data.setCompGroups, setErrorMess: props.data.setErrorMess}}/>
