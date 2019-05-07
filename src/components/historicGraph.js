733a7976b04002e282d62751c5cb2bcdeeb669a1
Eliance <eli.jarcet@free.fr>
2019-05-07 17:21:13 +0200

    dynamic values for historical graph

--- src/components/historicGraph.js
+++ src/components/historicGraph.js
@@ -3,53 +3,43 @@ import Chart from 'chart.js'
 
 export default (props) =>
   <div className='wrapperHistoricGraph'>
-    <canvas id='historicGraph' width='100' height='50'
-      oncreate={(element) => {
-        let ctx = element.getContext('2d')
-        const historicData = {
-          datasets: [{
-            label: 'historic graph',
-            data: [{
-              x: 2005,
-              y: 2
-            },
-            {
-              x: 2018,
-              y: 5
-            },
-            {
-              x: 2008,
-              y: 3
+    {props.data.historical.state && (
+      <canvas id='historicGraph' width='100' height='50'
+        oncreate={(element) => {
+          let ctx = element.getContext('2d')
+
+          const yLabels = props.data.historical.val.reduce((acc, n) => acc.indexOf(n.category) === -1 ? [n.category, ...acc] : [...acc], [])
+
+          const historicData = {
+            labels: props.data.historical.val.map(x => parseInt(x.year)).reduce((acc, n) => [n, ...acc], []),
+            datasets: [{
+              data: props.data.historical.val.map(e => { return {x: parseInt(e.year), y: yLabels.indexOf(e.category)} })
             }]
-          }]
-        }
+          }
 
-        console.log(props.data.historical.val)
+          // console.log(props.data.historical.val)
+          // console.log(yLabels)
 
-        const yLabels = {
-          0: 'LC', 1: 'NT', 2: 'VU', 3: 'EN', 4: 'CR', 5: 'EW', 6: 'EX'
-        }
-
-        const historicOptions = {
-          scales: {
-            yAxes: [{
-              ticks: {
-                callback: (value) => yLabels[value]
-              }
-            }]
-          },
-          elements: {
-            line: {
-              tension: 0
+          const historicOptions = {
+            fill: true,
+            scales: {
+              scaleLabel: {
+                display: true
+              },
+              yAxes: [{
+                ticks: {
+                  callback: (value) => yLabels[value]
+                }
+              }]
             }
           }
-        }
 
-        const historicGraph = new Chart(ctx, {
-          type: 'line',
-          data: historicData,
-          options: historicOptions
-        })
-      }}
-    />
+          const historicGraph = new Chart(ctx, {
+            type: 'line',
+            data: historicData,
+            options: historicOptions
+          })
+        }}
+      />
+    )}
   </div>
